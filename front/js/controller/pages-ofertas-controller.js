7e5f9f9940932b6d64e96f9be61f6d8840a7cf33
rodrigzer <rodrigzer@gmail.com>
2015-05-23 15:44:16 -0500

    agregando filtros y paginacion

--- front/js/controller/pages-ofertas-controller.js
+++ front/js/controller/pages-ofertas-controller.js
@@ -2,16 +2,17 @@
 (function(){
 	angular.module('app')
 		.controller('PageOfertasController', 
-			['$scope','$route','$http','$timeout',
-			function($scope,$route,$http,$timeout){
+			['$scope','$route','$http','$timeout','$routeParams',
+			function($scope,$route,$http,$timeout, $routeParams){
 				
 
 
 				$scope.items =[];
 
+				$scope.page = 1;
 
 				// $http({method:'POST', url:'data/ofertas.php', params:{slug:'all'}})				
-				$http({method:'GET', url:'http://172.28.13.120/cazaofertas/public/index.php/offer', params:{cat:'all'}})
+				$http({method:'GET', url:'http://172.28.13.120/cazaofertas/public/index.php/offer', params:{cat:$routeParams.categoria, page:$scope.page}})
 				.success(function(data){
 					// console.log($scope.items);
 
@@ -24,13 +25,14 @@
 				});
 
 				$scope.loadMore= function(){
-
+				$scope.page++;
+				console.log($scope.page);
 				$scope.setLoadingStatus(true);
-				$http({method:'GET', url:'http://172.28.13.120/cazaofertas/public/index.php/offer', params:{cat:'all'}})
+				$http({method:'GET', url:'http://172.28.13.120/cazaofertas/public/index.php/offer', params:{cat:$routeParams.categoria, page:$scope.page}})
 					.success(function(data){
 
 						$scope.items = $scope.items.concat(data);
-						
+
 						$scope.setLoadingStatus(false);
 
 						$timeout(function(){
